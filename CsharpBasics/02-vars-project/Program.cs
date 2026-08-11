6bd49cb9306f6959633faddd7587edb18765f492
vctrchappot <victor.chappot@digitecgalaxus.ch>
2026-08-11 13:07:49 +0200

    new branch Aufgabe 2

---
+++ CsharpBasics/02-vars-project/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _02_vars_project;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        piEulerNumber();
+    }
+    
+    static void NameAge()
+    {
+        byte age = 17;
+        string name = "Victor";
+
+        string agename = name + ", " + age;
+        Console.WriteLine(agename);
+    }
+
+    static void piEulerNumber()
+    {
+        const float piFloat = 3;
+        const double piDouble = 0.14159;
+        Console.WriteLine("Die ersten sechs Ziffern von Pi sind " + (piFloat + piDouble));
+    }
+}
+
