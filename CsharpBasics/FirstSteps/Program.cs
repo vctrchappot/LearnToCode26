6c03de70b4c547cb07a20da26ebf8bd8a75968c0
schwnby <simon.unger@digitecgalaxus.ch>
2026-08-11 11:13:11 +0200

    Create solution and first console application

---
+++ CsharpBasics/FirstSteps/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FirstSteps;
+
+static class Program
+{
+    public static void Main(string[] args)
+    {
+        var arguments = string.Join(",", args);
+        Console.WriteLine($"Here comes the arguments: {arguments}");
+    }
+}
